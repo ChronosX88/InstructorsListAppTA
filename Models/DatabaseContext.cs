c9de99b9db009d6e98bd2b7cb4ab7e6eb47d912b
ChronosX88 <chronosx88@gmail.com>
2019-09-24 22:37:09 +0400

    [backend] fix: Fix build error in DatabaseContext

--- Models/DatabaseContext.cs
+++ Models/DatabaseContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 namespace InstructorsListApp.Models {
     public class DatabaseContext : DbContext
     {
-        public DatabaseContext(DbContextOptions<ApplicationContext> options) 
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) 
             : base(options)
         { }
  
