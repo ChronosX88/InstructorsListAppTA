670c9a0b9ce1f9c25a1031fbbc1890551bbdab1f
ChronosX88 <chronosx88@gmail.com>
2019-09-24 21:26:29 +0400

    [ui] feat: Add table view (with test data)

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+    public instructors: any[] = [{id: "1", firstName: "Test", lastName: "Testov"}];
+
+    constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+        /*http.get<Instructor[]>(baseUrl + 'api/v1/instructors').subscribe(result => {
+          this.instructors = result;
+        }, error => console.error(error));*/
+    }
+}
+
+interface Instructor {
+    id: string,
+    firstName: string,
+    lastName: string
 }
